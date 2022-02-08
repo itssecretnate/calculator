fcb63fbdfc331ae438bed924ebf18e10148119e9
Nathan Mena <natemena153@gmail.com>
2022-02-08 16:24:15 -0700

    Clean up and fixed math for squareroot.

--- calc.js
+++ calc.js
@@ -18,20 +18,21 @@ reader.question("What would you like to calculate?: ", function(input){
 	// This line closes the connection to the command line interface.
 	reader.close()
 
-	if(tokens.length > 3) 
+	if(tokens.length > 3) //Check if there are more than 3 arguments. (For example: + 1 2 3)
 	{
-		console.log("Please only enter 3 arguments. (1 symbol and 2 numbers.)");
+		console.log("Please only enter 3 arguments. (1 symbol and 2 numbers)");
 		return;
 	}
 
-	if(mathSymbol == "+") console.log(num1 + num2);
-	if(mathSymbol == "-") console.log(num1 - num2);
-	if(mathSymbol == "*") console.log(num1 * num2);
+	if(mathSymbol == "+") console.log(num1 + num2); //Addition
+	if(mathSymbol == "-") console.log(num1 - num2); //Subtraction
+	if(mathSymbol == "*") console.log(num1 * num2); //Multiplication
+	if(mathSymbol == "sqrt") console.log(Math.sqrt(num1)); //Sqaureroot
+
+	//Division (With error checking to prevent division by 0)
 	if(mathSymbol == "/") {
 		if(num1 == 0 || num2 == 0) console.log("Cannot divide by 0.");
 		else console.log(num1 / num2);
 	}
-
-	if(mathSymbol == "sqrt") console.log(num1 * num1); //Times the first number to iteslf to get a squareroot
 
 });
